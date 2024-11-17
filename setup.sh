4ab8dbfa38a243d89562dbf15d13b0c54b0fed1e
Rambo <ramborigs90@gmail.com>
2024-11-17 17:45:09 +0000

    added custom motd

--- setup.sh
+++ setup.sh
@@ -146,6 +146,29 @@ echo "#TABBY WORKING DIR SCRIPT" >> /home/$user/.bashrc
 echo "export PS1=\"\$PS1\[\e]1337;CurrentDir=\"'/home/$user\a\]'" >> /home/$user/.bashrc
 echo "Done."
 
+
+printf "$ST Customizing motd \n $SB"
+
+# Install prerequisites
+apt update && apt dist-upgrade -y
+apt install lolcat linuxlogo toilet figlet cowsay fortune -y
+
+# Backup up original motd
+mv /etc/motd /etc/motd.bak
+
+# Comment out original linux os and kernel info
+sed -i -e 's/uname -snrvm/#uname -snrvm/g' /etc/update-motd.d/10-uname
+
+# Create custom message
+touch /etc/update-motd.d/99-custom-motd
+echo "#!/bin/bash" >> /etc/update-motd.d/99-custom-motd
+echo "{" >> /etc/update-motd.d/99-custom-motd
+echo "  /usr/games/fortune | /usr/games/cowsay -f tux" >> /etc/update-motd.d/99-custom-motd
+echo "  toilet -f ivrit \"$HOSTNAME\"" >> /etc/update-motd.d/99-custom-motd
+echo "  linuxlogo -a -g -u -d -s -k -F \"Debian $(cat /etc/debian_version) Bookworm \n#O Kernel #V \n#M #T #R RAM \n#U\"" >> /etc/update-motd.d/99-custom-motd
+echo "} | /usr/games/lolcat -p 13 --force" >> /etc/update-motd.d/99-custom-motd
+chmod +x /etc/update-motd.d/99-custom-motd
+
 # Check for panel option
 case $panel in
     cloudpanel)
